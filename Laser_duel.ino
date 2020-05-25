15aa70d88495609f0f4333ed3d356d06cc003048
Samuel Li <44010657+exiaruler@users.noreply.github.com>
2020-05-26 03:18:32 +1000

    Update Laser_duel.ino

--- Laser_duel.ino
+++ Laser_duel.ino
@@ -11,3 +11,25 @@ void loop() {
 void timer(){
    
 }
+
+void trigger(){
+  //use the sensor 
+   if(buttonState==HIGH&&count!=0){
+     digitalWrite(LED, HIGH);
+      count--;
+      delay(500);
+    }else
+        digitalWrite(LED, LOW);
+    lcd.print("Engaged");
+      lcd.setCursor(0,1);
+      lcd.print("Counts left ");
+    lcd.print(count);
+  
+    //When there no ammmo
+         if(buttonState==HIGH&&count==0){
+           lcd.clear();
+           lcd.print("Out of counts");
+     delay(1000);
+           lcd.clear();                           
+    }
+}
