17276f8b6dd0b2b1c2e4597f628f5ae5cee83a48
Samuel Li <44010657+exiaruler@users.noreply.github.com>
2020-06-05 00:47:43 +1000

    Add files via upload
    Not functional final prototype

--- Laser_duel.ino
+++ Laser_duel.ino
@@ -9,9 +9,9 @@
 #define BIT_DATA        6
 #include <Wire.h>
 #include "rgb_lcd.h"
- 
+ #include <HCSR04.h> //ultrasonic distance sensor 
 rgb_lcd lcd;
- 
+UltraSonicDistanceSensor distanceSensor(6, 7);
 const int colorR = 255;
 const int colorG = 0;
 const int colorB = 0;
@@ -20,9 +20,14 @@ const int ir_freq = 38;                 // 38k
 unsigned char dtaSend[20];
 
 const int pinRecv = 2;       // ir receiver connect to D2
+int MoPin = 5; //vibrator
+
 //game system
 int health=1; 
-int ammo=1; 
+int ammo=1;
+long randNumber;
+double check = 300; 
+    
 //transmitted data
 void dtaInit() {
     dtaSend[BIT_LEN]        = 11;      // all data that needs to be sent
@@ -46,12 +51,13 @@ void setup() {
     IR.Init(pinRecv);
     Serial.println("init over");
      dtaInit();
-
+   pinMode( MoPin, OUTPUT );//vibrator
      paj7620Init(); //Gesture controller 
 uint8_t error = 0;
 //LCD
 lcd.begin(16, 2);
  lcd.setRGB(colorR, colorG, colorB);
+ 
 
 }
 unsigned char dta[20];//dta array
@@ -59,6 +65,8 @@ unsigned char dta[20];//dta array
 void loop() {
 uint8_t data = 0, data1 = 0, error;
     error = paj7620ReadReg(0x43, 1, &data);  
+  //starting session declare button 
+  
     //Game session
     while(health!=0){
        lcd.setCursor(1,0);
@@ -71,28 +79,131 @@ uint8_t data = 0, data1 = 0, error;
             case GES_UP_FLAG:
                 delay(800);
                 paj7620ReadReg(0x43, 1, &data);
-                if (data == GES_FORWARD_FLAG) {
+                if (data == GES_FORWARD_FLAG&&ammo!=0) {
                 } else {
+                      //double distance = distanceSensor.measureDistanceCm();
+                      
                       IR.Send(dtaSend, 38);
+                      ammo--; 
                     Serial.println("send");
+                      
                 }
                 break;
+               
            
         }
     }
     delay(100);
 
-    
+     if(ammo==0){
+     reload(); 
+                }
   //Gettting hit
     if (IR.IsDta()) {
     // get IR data
     lcd.clear();
         IR.Recv(dta); // receive data to dta
         health--;
+        alert(); 
   lcd.print("game over");
   break;
     }
     }
+    //if button is pressed resets the game 
+    while(health==0){
+     
+    }
 }
+
+void start(){
+   lcd.clear();
+    lcd.print("5");
+    delay(1000);
+    lcd.clear();
+    lcd.print("4");
+    delay(1000);
+    lcd.clear();
+    lcd.print("3");
+    delay(1000);
+    lcd.clear();
+    lcd.print("2");
+    delay(1000);
+    lcd.clear();
+    lcd.print("1");
+    delay(1000);
+    lcd.clear();
+    lcd.print("Start");
+    delay(1000);
+    lcd.clear();
+}
+
+void reload(){
+   lcd.print("Reload time:");
+    int randNumber = random(1,6);
+    lcd.setCursor(13,0);
+    lcd.print(randNumber);
+    lcd.setCursor(0,1); 
+    switch(randNumber){
+      
+      case 5:
+      lcd.print(randNumber);
+      delay(1000);
+      randNumber--;
+      lcd.clear();
+
+      case 4:
+      lcd.print(randNumber);
+      delay(1000);
+      randNumber--;
+      lcd.clear();
+
+      case 3:
+      lcd.print(randNumber);
+      delay(1000);
+      randNumber--;
+      lcd.clear();
+
+      case 2:
+      lcd.print(randNumber);
+      delay(1000);
+      randNumber--;
+      lcd.clear();
+
+      case 1:
+      lcd.print(randNumber);
+      delay(1000);
+      lcd.clear();
+      break;
+
+      ammo++; 
+      
+}
+}
+void alert(){
+    digitalWrite(MoPin, HIGH);
+    delay(1000);
+ 
+    digitalWrite(MoPin, LOW);
+    delay(1000);
+}
+
+void distance(){
+  Serial.println(distanceSensor.measureDistanceCm());
+    delay(500);
+    
+    double check = 300; 
+    double distance = distanceSensor.measureDistanceCm();
+    if (distance < check)
+    {
+      Serial.println("True"); 
+    }
+    else
+    {
+      Serial.println("False");
+    }
+}
+
+
+
 
  
